0f8649af87f2accdd7046c2da7a9e83c4851e14c
Lucas Felipe Costa <lucasfelipecost4@gmail.com>
2025-06-26 21:00:01 -0300

    Implements item deletion from the cart

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import { addItem, calculateTotal, listarItens } from "./services/cart.js";
+import { addItem, deleteItem, listarItens } from "./services/cart.js";
 import { createItem } from "./services/item.js";
 
 const cart = [];
 
 await addItem(cart, await createItem("hotwhells ferrari", 20.01, 1));
 await addItem(cart, await createItem("hotwhells lamborguini", 15.19, 2));
-listarItens(cart);
+await listarItens(cart);
+
+await deleteItem(cart, cart[0].name);
+await listarItens(cart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,17 +1,17 @@
 async function addItem(userCart, item) {
   userCart.push(item);
-  return userCart;
 }
-async function deleteItem(userCart, nameItem) {}
+async function deleteItem(userCart, nameItem) {
+  const index = userCart.findIndex((item) => item.name === nameItem);
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  }
+}
 async function removeItem(userCart, index) {}
 
 async function listarItens(userCart) {
   userCart.forEach((item) => {
-    console.log(
-      `${item.name}:\t ${item.quantity} x R$ ${
-        item.price
-      } = R$ ${item.subTotal()}`
-    );
+    console.log(`${item.name}:\t ${item.quantity} x R$ ${item.price} = R$ ${item.subTotal()}`);
   });
   console.log(`Valor total:\t\t\t\tR$ ${await calculateTotal(userCart)}`);
 }
@@ -19,4 +19,4 @@ async function calculateTotal(userCart) {
   return userCart.reduce((total, item) => total + item.subTotal(), 0);
 }
 
-export { addItem, deleteItem, removeItem, calculateTotal, listarItens };
+export { addItem, calculateTotal, deleteItem, listarItens, removeItem };
